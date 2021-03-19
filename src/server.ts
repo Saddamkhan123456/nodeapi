6d7a2bf9358694c148c129e94b9a5dd65bc4ceae
Saddamkhan12346 <khansaddam444@gmail.com>
2021-03-19 17:52:03 +0530

    port setup

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,21 @@
-import { Express } from 'express';
+import * as dotenv from 'dotenv';
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+dotenv.config();
+
+const app = express();
+
+// app configration
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+const PORT = process.env.PORT || '5000';
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+  });
